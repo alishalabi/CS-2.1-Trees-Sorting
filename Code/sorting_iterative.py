35dfef7c19f728e01df0aa6cc623e79dc56356f8
Ali Shalabi <ashalabi13@gmail.com>
2019-10-24 14:11:53 -0700

    Finished initial logic for selection sort, about to begin testing

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -10,7 +10,7 @@ def is_sorted(items):
     # Iterate through each item in array, omit final item
     for index in range(len(items) - 1):
         # Check if item is greater than or equal to following item (ie not in order)
-        if items[index] >= items[index + 1]:
+        if items[index] > items[index + 1]:
             # Early exit
             return False
     # All items in order
@@ -22,14 +22,15 @@ def bubble_sort(items):
     repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
+    # Repeat until all items are in sorted order
     # Iterate through each index in array
     for current_index in range(len(items) - 1):
         # Sub-iterate through each index up until target index
         for index in range(current_index):
+            # If item is out of order
             if items[index] > items[index + 1]:
-
-                # TODO: Swap adjacent items that are out of order
+                # Swap adjacent items that are out of order
+                items[index], items[index + 1] = items[index + 1], items[index]
 
 
 def selection_sort(items):
@@ -37,9 +38,25 @@ def selection_sort(items):
     unsorted item, and repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    # Set counter
+    counter = 0
+    while counter < len(items):
+        # Set lowest item of iteration
+        current_lowest_item = items[counter]
+        # Set index of remaining items
+        current_lowest_index = counter
+        # Iterate through remaining items
+        for remaining_item in len([counter:len(items)]):
+            # See if item is lower than current lowest
+            if remaining_item < current_lowest:
+                # Set current lowest value
+                current_lowest_item = remaining_item
+                # Set current lowest index
+                current_lowest_index = items.index(remaining_item)
+        # Swap index at counter with current lowest
+        items[counter], items[current_lowest_index] = items[current_lowest_index], items[counter]
+        # Increment counter
+        counter += 1
 
 
 def insertion_sort(items):
